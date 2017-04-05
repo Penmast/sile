990986f331ca3da0004c8eee51bce60840b39768
David Deray <deray@edu.ece.fr>
2017-04-05 11:04:33 +0200

    Libraries cleanup

--- WindowsFormsApplication1/menu.cs
+++ WindowsFormsApplication1/menu.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApplication1
         public ecranprincipal()
         {
             InitializeComponent();
+            new HandRecognitionPicture("aa");
         }
 
         private void Form1_Load(object sender, EventArgs e)
